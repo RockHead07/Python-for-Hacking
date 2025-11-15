618ab2cc5b9892fd9196defdad8eca613acf3704
RockHead07 <dana.bagus07@gmail.com>
2025-11-15 16:18:03 +0700

    Yes

--- tcpSocket.py
+++ tcpSocket.py
@@ -1,11 +1,11 @@
 import socket
 
 targetHost = "www.google.com"
-targertPort = 80
+targetPort = 80
 
-# Membuat socket 
-client = socket.socket(socket.AF_inet, socket.SOCK_STREAM)
-client.connect((targetHost, targertPort))  
+# Membuat socket
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((targetHost, targetPort))
 
 client.send(b"GET / HTTP/1.1\r\nHost: google.com\r\n\r\n")
 
