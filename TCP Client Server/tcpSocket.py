30eaab1327e4516f46c8bd3328145b6d5fd4a25b
RockHead07 <dana.bagus07@gmail.com>
2025-11-15 19:19:08 +0700

    TCP Client Server: Update tcpServer.py and tcpSocket.py

--- TCP Client Server/tcpSocket.py
+++ TCP Client Server/tcpSocket.py
@@ -1,16 +1,14 @@
 import socket
 
-targetHost = "www.google.com"
-targetPort = 80
+if __name__ == "__main__":
+    ip = "127.0.0.1"
+    port = 12345
 
-# Membuat socket
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((targetHost, targetPort))
-
-client.send(b"GET / HTTP/1.1\r\nHost: google.com\r\n\r\n")
-
-# Menerima respon dari server
-response = client.recv(4096)
-print(response.decode())
-
-client.close()
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.connect((ip, port))
+    
+    string = input("Enter a message to send to the server: ")
+    server.send(bytes(string, 'utf-8'))
+    buffer = server.recv(1024)
+    buffer = buffer.decode("utf-8")
+    print(f"Server response: {buffer}")
